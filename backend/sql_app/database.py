9fcccc671842f56a4a31cfc7039619dc9538571d
Tamara Gunkel <tamara.gunkel@web.de>
2021-09-24 21:45:01 +0200

    fix database connection

--- backend/sql_app/database.py
+++ backend/sql_app/database.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # ToDo add credentials to .env 
-SQLALCHEMY_DATABASE_URL = "postgresql:////user:example@localhost:5432/db"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://user:example@localhost:5432/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
