0924fadd15e748d2c3ea89c2f638b4034e7f6ee1
David Scheidt <scheidt.dav@gmail.com>
2021-09-24 21:09:17 +0200

    Disable dark mode for the time being

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,6 +1,8 @@
 import {extendTheme} from '@chakra-ui/react';
 
 const theme = extendTheme({
+  initialColorMode: 'light',
+  useSystemColorMode: false,
   fonts: {
     body: 'Lato, sans-serif',
     heading: 'Lato, sans-serif',
