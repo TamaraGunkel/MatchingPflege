590ff72f815a54fd9b59f5026a6b7ab5dfa39f3b
David Scheidt <scheidt.dav@gmail.com>
2021-09-24 21:11:07 +0200

    Remove Who Cares text in favour of app bar

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,43 +1,36 @@
 import {Link} from 'react-router-dom';
 import {Box, SimpleGrid, Heading, Center, LinkOverlay, LinkBox} from '@chakra-ui/react';
 
-function LandingPage(props) {
-  return (
-    <div>
-      <Center>
-        <Heading>Who Cares?</Heading>
-      </Center>
-      <SimpleGrid columns={2} spacing={10}>
-        <LinkBox>
-          <Link to="/login">
-            <LinkOverlay>
-              <Box height="auto" p="50px" bg="tomato">
-                <Center>
-                  <Heading centerContent size="lg">
-                    Login für Pflegedienste
-                  </Heading>
-                </Center>
-              </Box>
-            </LinkOverlay>
-          </Link>
-        </LinkBox>
+const LandingPage = () => (
+  <SimpleGrid columns={2} spacing={10}>
+    <LinkBox>
+      <Link to="/login">
+        <LinkOverlay>
+          <Box height="auto" p="50px" bg="tomato">
+            <Center>
+              <Heading centerContent size="lg">
+                Login für Pflegedienste
+              </Heading>
+            </Center>
+          </Box>
+        </LinkOverlay>
+      </Link>
+    </LinkBox>
 
-        <LinkBox>
-          <Link to="/create-inquiry">
-            <LinkOverlay>
-              <Box height="auto" p="50px" bg="tomato">
-                <Center>
-                  <Heading centerContent size="lg">
-                    Anfrage erstellen
-                  </Heading>
-                </Center>
-              </Box>
-            </LinkOverlay>
-          </Link>
-        </LinkBox>
-      </SimpleGrid>
-    </div>
-  );
-}
+    <LinkBox>
+      <Link to="/create-inquiry">
+        <LinkOverlay>
+          <Box height="auto" p="50px" bg="tomato">
+            <Center>
+              <Heading centerContent size="lg">
+                Anfrage erstellen
+              </Heading>
+            </Center>
+          </Box>
+        </LinkOverlay>
+      </Link>
+    </LinkBox>
+  </SimpleGrid>
+);
 
 export default LandingPage;
