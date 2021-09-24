c6368b714279631d696ad6f337f103eeb12347cb
David Scheidt <scheidt.dav@gmail.com>
2021-09-24 21:06:33 +0200

    Add small little frame for all pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {VStack, Box, Heading} from '@chakra-ui/react';
 
 import LandingPage from './components/LandingPage/LandingPage';
 import Inquiries from './components/Inquiries/Inquiries';
@@ -9,25 +10,30 @@ import Success from './components/Success/Success';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/login">
-          <LoginForm />
-        </Route>
-        <Route path="/inquiries">
-          <Inquiries />
-        </Route>
-        <Route path="/create-inquiry">
-          <CreateInquiry />
-        </Route>
-        <Route path="/success">
-          <Success />
-        </Route>
-        <Route path="/">
-          <LandingPage />
-        </Route>
-      </Switch>
-    </Router>
+    <VStack w="100vw" p="0">
+      <Box p={8} boxShadow="lg" w="100%">
+        <Heading size="lg">Zuhause in Münster</Heading>
+      </Box>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <LoginForm />
+          </Route>
+          <Route path="/inquiries">
+            <Inquiries />
+          </Route>
+          <Route path="/create-inquiry">
+            <CreateInquiry />
+          </Route>
+          <Route path="/success">
+            <Success />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </Router>
+    </VStack>
   );
 }
 
