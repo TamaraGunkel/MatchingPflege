751e16a120ef90f67619a05132ed2bc3a6482eee
David Scheidt <scheidt.dav@gmail.com>
2021-09-24 20:07:13 +0200

    Remove auto-generated css and images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
-import logo from './logo.svg';
-import LandingPage from './components/LandingPage/LandingPage'
-import {ChakraProvider} from "@chakra-ui/react"
-import { Container } from "@chakra-ui/react"
+import LandingPage from './components/LandingPage/LandingPage';
+import {ChakraProvider} from '@chakra-ui/react';
+import {Container} from '@chakra-ui/react';
 function App() {
   return (
-      <ChakraProvider>
-          <div className="App">
-              <header className="App-header">
-                  <Container>
-                      <LandingPage></LandingPage>
-                  </Container>
-              </header>
-          </div>
-      </ChakraProvider>
+    <ChakraProvider>
+      <div className="App">
+        <header className="App-header">
+          <Container>
+            <LandingPage></LandingPage>
+          </Container>
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
