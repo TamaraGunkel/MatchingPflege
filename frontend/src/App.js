95658b38c2e8c7ca50c2a718a1caedb6be821eea
David Scheidt <scheidt.dav@gmail.com>
2021-09-24 20:18:02 +0200

    Add routes, fix small missing logo in login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import * as React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import LandingPage from './components/LandingPage/LandingPage';
+import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/login"></Route>
+        <Route path="/login">
+          <LoginForm />
+        </Route>
         <Route path="/inquiries"></Route>
         <Route path="/create-inquiry"></Route>
         <Route path="/success"></Route>
