4fb6b7dcf583c070f744e421c3b21983fda5b95c
Jens Könning <creeper00001@googlemail.com>
2021-09-25 14:14:57 +0200

    temp commit inquiries site

--- frontend/src/components/Inquiries/Inquiries.js
+++ frontend/src/components/Inquiries/Inquiries.js
@@ -1,5 +1,45 @@
+import * as React from 'react';
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+  Box,
+  Heading,
+  SimpleGrid
+} from "@chakra-ui/react"
+
+
 function Inquiries(props) {
-  return <div>Inquiries</div>;
+
+  const [inquires, setInquires] = React.useState([]);
+  React.useEffect(() => {}, []);
+
+  return (
+      <Box p={12} w="100%">
+        <Heading mb={12} size="lg">Verwaltung aller Anfragen</Heading>
+        <Tabs isFitted="true" variant="enclosed">
+          <TabList>
+            <Tab>Offene Anfragen</Tab>
+            <Tab>In Akquise</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <SimpleGrid columns={1} spacing={10}>
+                inquires.map((key, index) => {
+                <Box bg="tomato" height="100px"></Box>
+              }
+              </SimpleGrid>
+            </TabPanel>
+            <TabPanel>
+              <p>two</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+  );
 }
 
 export default Inquiries;
