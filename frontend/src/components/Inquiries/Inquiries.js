d9c285eb0859f6e5f6a670908c351767681a0cf9
Jens Könning <creeper00001@googlemail.com>
2021-09-25 15:15:14 +0200

    temp uebersichtseite inquires

--- frontend/src/components/Inquiries/Inquiries.js
+++ frontend/src/components/Inquiries/Inquiries.js
@@ -7,14 +7,26 @@ import {
   TabPanels,
   Box,
   Heading,
-  SimpleGrid
-} from "@chakra-ui/react"
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td, Link, Button,
 
+} from "@chakra-ui/react"
+import {Link as RouterLink} from "react-router-dom";
 
 function Inquiries(props) {
 
   const [inquires, setInquires] = React.useState([]);
-  React.useEffect(() => {}, []);
+
+  React.useEffect(async() => {
+    await fetch("/inquiries?page=5&page_size=5&district=5&status=1")
+        .then(response => response.json())
+        .then(data => setInquires(data))
+  }, []);
 
   return (
       <Box p={12} w="100%">
@@ -27,11 +39,37 @@ function Inquiries(props) {
 
           <TabPanels>
             <TabPanel>
-              <SimpleGrid columns={1} spacing={10}>
-                inquires.map((key, index) => {
-                <Box bg="tomato" height="100px"></Box>
-              }
-              </SimpleGrid>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Stadtteil</Th>
+                    <Th>Art der Pflege</Th>
+                    <Th>Details</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {inquires.map(inquiry =>
+                  <Tr>
+                    <Td>inquiry.address.district</Td>
+                    <Td>inquiry.services</Td>
+                    <Td>
+                      <Link as={RouterLink} to={"/inquiry/"+inquiry.id} alignSelf="flex-end">
+                      <Button variant="solid">
+                        Mehr Informationen
+                      </Button>
+                    </Link>
+                   </Td>
+                  </Tr>
+                  )}
+                </Tbody>
+                <Tfoot>
+                  <Tr>
+                    <Th>Stadtteil</Th>
+                    <Th>Art der Pflege</Th>
+                    <Th>Details</Th>
+                  </Tr>
+                </Tfoot>
+              </Table>
             </TabPanel>
             <TabPanel>
               <p>two</p>
