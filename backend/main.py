b5ccbfb0a53e269d0cffcfc8a1119ae40edd4988
Daniel Dahme <d.dahme@foodtracks.de>
2021-09-25 11:51:44 +0200

    add cors allow origin

--- backend/main.py
+++ backend/main.py
@@ -13,6 +13,18 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins =[
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 def get_db():
     db = SessionLocal()
     try:
