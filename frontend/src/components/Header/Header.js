2982770253007857198703724f94b6a437ff440d
David Scheidt <scheidt.dav@gmail.com>
2021-09-25 13:29:45 +0200

    Add routing to logo

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,4 +1,15 @@
-import {Box, Flex, Spacer, Heading, Link, Button, HStack, Image} from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Spacer,
+  Heading,
+  Link,
+  Button,
+  HStack,
+  Image,
+  LinkOverlay,
+  LinkBox,
+} from '@chakra-ui/react';
 import {useLocation, Link as RouterLink} from 'react-router-dom';
 
 import Logo from '../../img/WhoCaresLogoV2.png';
@@ -9,10 +20,18 @@ const Header = () => {
   return (
     <Box p={8} boxShadow="md" w="100%" as="header">
       <Flex>
-        <HStack spacing={6}>
-          <Image src={Logo} boxSize="24" />
-          <Heading size="lg">Zuhause in Münster</Heading>
-        </HStack>
+        <LinkBox>
+          <HStack spacing={6}>
+            <Link as={RouterLink} to="/" alignSelf="center">
+              <LinkOverlay>
+                <Image src={Logo} boxSize="24" />
+              </LinkOverlay>
+            </Link>
+            <Heading size="lg" textDecoration="none">
+              Zuhause in Münster
+            </Heading>
+          </HStack>
+        </LinkBox>
         <Spacer />
         {location.pathname === '/' && (
           <Link as={RouterLink} to="/login" alignSelf="center">
